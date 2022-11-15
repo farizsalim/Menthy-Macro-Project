b98634bb0355762fa733c28de4d22b33b2c4bfec
farizsalim <farizsalim10@yahoo.co.id>
2022-11-15 10:28:58 +0800

    Project-Macro-Group 3 Web

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navs from "./components/Navigation";
+import Caros from "./components/Carousel";
+import Searchinput from "./components/Search";
+// import {Routes, Route} from "react-router-dom";
+import "./App.css";
+
+const App = () => {
+  return (
+      <div className="back-color">
+        <Navs/>
+        <Caros/>
+        <Searchinput/>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/components/Imagesrc.js
@@ -0,0 +1,9 @@
+import caros1 from "../images/caros1.png"
+import caros2 from "../images/caros2.png"
+import caros3 from "../images/caros3.png"
+
+const Caros1 = caros1;
+const Caros2 = caros2;
+const Caros3 = caros3;
+
+export {Caros1,Caros2,Caros3};
